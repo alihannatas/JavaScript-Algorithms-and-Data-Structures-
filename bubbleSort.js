a506215305fa2a808b2ffb3bae3fd842c53981b4
alihannatas <alihan2644@gmail.com>
2023-12-14 15:15:36 +0300

    Bubble Sort added.

---
+++ bubbleSort.js
@@ -0,0 +1,29 @@
+//basic version
+const basicBubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+//optimizated
+const bubbleSort = (arr) => {
+  let swap;
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = i - 1; j < arr.length; j++) {
+      swap = false;
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swap = true;
+      }
+    }
+    if (swap) break;
+  }
+  return arr;
+};
