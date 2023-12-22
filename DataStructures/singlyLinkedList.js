155a067cd1d243b611bfad2ec9e688bd81eecb83
alihannatas <alihan2644@gmail.com>
2023-12-22 19:12:21 +0300

    Get, set, insert, remove and remove functions were written for the singly linked list.

--- DataStructures/singlyLinkedList.js
+++ DataStructures/singlyLinkedList.js
@@ -64,4 +64,62 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let iterator = 0;
+    let current = this.head;
+    while (iterator !== index) {
+      current = current.next;
+      iterator++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (node) {
+      node.val = val;
+      return true;
+    }
+    return null;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index == this.length) return !!this.push(val);
+    if (index == 0) return !!this.unshift(val);
+    const newNode = new Node(val);
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.next = afterNode;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return false;
+    if (index == 0) return !!this.shift();
+    if (index + 1 == this.length) return !!this.pop();
+    let beforeNode = this.get(index - 1);
+    let removedNode = beforeNode.next;
+    removedNode.next = beforeNode.next;
+    this.length--;
+    return removedNode;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
