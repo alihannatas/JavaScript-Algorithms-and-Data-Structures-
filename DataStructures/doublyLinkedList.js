98abe6b3058a9fd6671f9a889793c36e9be86fae
alihannatas <alihan2644@gmail.com>
2023-12-24 16:24:23 +0300

    Get, set, insert and remove functions were written for the doubly linked list.

--- DataStructures/doublyLinkedList.js
+++ DataStructures/doublyLinkedList.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-class DoubleLinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -64,5 +64,62 @@ class DoubleLinkedList {
     this.head = node;
     this.length++;
     return node;
+  }
+
+  get(index) {
+    if (index < 0 || this.length <= index) return null;
+    let distanceIndex = this.length - index;
+    let current;
+    if (index >= this.length / 2) {
+      current = this.tail;
+      while (distanceIndex - 1 > 0) {
+        current = current.prev;
+        distanceIndex--;
+      }
+    } else {
+      current = this.head;
+      while (index > 0) {
+        current = current.next;
+        index--;
+      }
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let current = this.get(index);
+    if (!current) return false;
+    current.val = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index < 0 || this.length < index) return false;
+    if (index == 0) return !!this.unShift(val);
+    if (index == this.length) return !!this.push(val);
+    let node = new Node(val);
+    let prevNode = this.get(index - 1);
+    let nextNode = prevNode.next;
+    prevNode.next = node;
+    node.next = nextNode;
+    node.prev = prevNode;
+    nextNode.prev = node;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index == 0) return this.shift();
+    if (index == this.length - 1) return !this.pop();
+    let prevNode = this.get(index - 1);
+    let currentNode = prevNode.next;
+    let nextNode = currentNode.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+    currentNode.next = null;
+    currentNode.prev = null;
+    this.length--;
+    return currentNode;
   }
 }
