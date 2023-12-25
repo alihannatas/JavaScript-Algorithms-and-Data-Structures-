e46937f1aa85a797ae632feb7d7b6ddc07ad435b
alihannatas <alihan2644@gmail.com>
2023-12-25 15:41:32 +0300

    Stack, push and pop functions have been added.

---
+++ DataStructures/stack.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size == 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+    return true;
+  }
+  pop() {
+    if (this.size == 0) return null;
+    let temp = this.first;
+    if (this.size == 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    console.log(temp);
+    temp.next = null;
+    console.log(temp);
+    this.size--;
+    return temp.value;
+  }
+}
