f780b17a781e2c4498e98dc7935284f2f10cf92e
Alihan Atas <46502610+alihannatas@users.noreply.github.com>
2024-01-21 16:09:54 +0300

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,10 +1,13 @@
 const selectionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
+    let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        //swap
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+      if (arr[j] > arr[min]) {
+        min = j;
       }
+    }
+    if (min != i) {
+      [arr[i], arr[min]] = [arr[min], arr[j]];
     }
   }
   return arr;
